5f55ed31b6f03ffd08c99f8430ee00bf741299fd
rush1dan <rushidan10@gmail.com>
2023-01-08 19:10:26 +0600

    Initial main window with menubar

---
+++ main.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+import tkinter.filedialog as tk_filedialog
+
+window = tk.Tk()
+
+#Main Window:
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+window_aspect_ratio = 1.5
+window_width = int(screen_width / 6)
+window_height = int(window_width / window_aspect_ratio)
+window.geometry(f"{window_width}x{window_height}")
+window.resizable(0, 0)
+
+#Menubar:
+menubar = tk.Menu(window)
+filemenu = tk.Menu(menubar, tearoff=0)
+filemenu.add_command(label="New", command=None)
+filemenu.add_command(label="Open", command=None)
+filemenu.add_command(label="Save", command=None)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=window.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+window.config(menu=menubar)
+
+window.mainloop()
