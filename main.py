cec6363103bf9b24714ee468f9f28e4888c2fc08
rush1dan <rushidan10@gmail.com>
2023-01-08 19:32:55 +0600

    Drag and drop capability added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 import tkinter.filedialog as tk_filedialog
+from tkinterdnd2 import DND_FILES, TkinterDnD
 
-window = tk.Tk()
+window = TkinterDnD.Tk()
 
 #Main Window:
 screen_width = window.winfo_screenwidth()
@@ -16,11 +17,14 @@ window.resizable(0, 0)
 menubar = tk.Menu(window)
 filemenu = tk.Menu(menubar, tearoff=0)
 #filemenu.add_command(label="New", command=None)
-filemenu.add_command(label="Open", command=None)
+filemenu.add_command(label="Open", command=lambda: print("open"))
 #filemenu.add_command(label="Save", command=None)
 filemenu.add_separator()
 filemenu.add_command(label="Exit", command=window.quit)
 menubar.add_cascade(label="File", menu=filemenu)
 window.config(menu=menubar)
+
+window.drop_target_register(DND_FILES)
+window.dnd_bind('<<Drop>>', lambda e: print(e.data))
 
 window.mainloop()
