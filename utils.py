7ec3ddbf081c7207b70c18c1353adfa3e4717987
rush1dan <rushidan10@gmail.com>
2023-01-10 23:36:31 +0600

    All files and folders extracted properly from dnd data

--- utils.py
+++ utils.py
@@ -5,21 +5,21 @@ def process_dnd_data(data: str):
     non_braced_paths_string = data
     for braced_path in braced_paths:
         non_braced_paths_string = non_braced_paths_string.replace(braced_path, "")
-        #bug: results in empty strings
 
-    non_braced_paths_list = non_braced_paths_string.split(" ")
-    print("non_braced_paths: \n")
-    print(non_braced_paths_list)
+    non_braced_paths_list = non_braced_paths_string.split()
 
-    braces_stripped_paths_list = remore_braces_from_paths(braced_paths=braced_paths)
-    print("\n braced_paths_stripped: \n")
-    print(braces_stripped_paths_list)
+    braces_stripped_paths_list = remove_braces_from_paths(braced_paths=braced_paths)
+
+    all_files = []
+    all_files.extend(non_braced_paths_list)
+    all_files.extend(braces_stripped_paths_list)
+    print(all_files)
     
 
 def get_braced_paths(data: str) -> list[str]:
     paths_surrounded_by_braces = re.findall(r'\{.*?\}', data)
     return paths_surrounded_by_braces
 
-def remore_braces_from_paths(braced_paths: list[str]) -> list[str]:
+def remove_braces_from_paths(braced_paths: list[str]) -> list[str]:
     braceless_paths = [path.strip(r"{}") for path in braced_paths]
     return braceless_paths
