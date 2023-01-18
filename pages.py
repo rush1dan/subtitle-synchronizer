fc578e64674ac0728fb6dd408d47aafbe1bcea9e
rush1dan <rushidan10@gmail.com>
2023-01-18 20:38:33 +0600

    Operation page gird layout WIP with list box and scroll view implemented properly

--- pages.py
+++ pages.py
@@ -62,13 +62,30 @@ class Operation_Page(Page):
         super().__init__(master)
 
     def show(self):
-        lbox_files = tk.Listbox(master=self, height=int(self.window_height * 0.5), width=int(self.window_width * 0.8), font=("Arial", 10))
+        self.rowconfigure(0, minsize=int(self.window_height * 0.4))
+        self.rowconfigure(1, minsize=int(self.window_height * 0.6))
+        self.columnconfigure(0, minsize=int(self.window_width))
+
+        frm_upper = tk.Frame(master=self, relief=tk.FLAT, borderwidth=0)
+        frm_upper.grid(row=0, column=0, sticky=tk.NSEW)
+
+        frm_upper.columnconfigure(0, minsize=int(self.window_width * 0.95))
+        frm_upper.columnconfigure(1, minsize=int(self.window_width * 0.05))
+        frm_upper.rowconfigure(0, minsize=int(self.window_height * 0.4))
+
+        frm_lbox_files = tk.Frame(master=frm_upper, relief=tk.FLAT, borderwidth=0)
+        frm_lbox_files.grid(row=0, column=0, sticky=tk.NSEW)
+
+        lbox_files = tk.Listbox(master=frm_lbox_files, width=1, height=1, font=("Arial", 8), justify="left")
+        for i, file in enumerate(Data.files):
+            lbox_files.insert(i + 1, f"{i + 1}. {file}")
         lbox_files.pack(side = "left", fill = "both", expand=True)
 
-        scrollbar = tk.Scrollbar(lbox_files)
+        frm_scrollbar = tk.Frame(master=frm_upper, relief=tk.FLAT, borderwidth=0)
+        frm_scrollbar.grid(row=0, column=1, sticky=tk.NSEW)
+
+        scrollbar = tk.Scrollbar(frm_scrollbar)
         scrollbar.pack(side = "right", fill = "both")
-        for i, file in enumerate(Data.files):
-            lbox_files.insert(i + 1, file)
 
         lbox_files.config(yscrollcommand = scrollbar.set)
         scrollbar.config(command = lbox_files.yview)
